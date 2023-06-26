777e1cc362b1ad8cefb5aca3ce94e801970483cf
ALVICELL <136303773+ALVICELL@users.noreply.github.com>
2023-06-26 08:36:29 +0700

    add-ns.sh

--- ssh/add-ns.sh
+++ ssh/add-ns.sh
@@ -56,13 +56,13 @@ function cert() {
 }
 clear
 function add-name() {
-    DOMAINNS=alvicell.my.id
+    DOMAINNS="alvicell.my.id"
     DAOMIN=$(cat /etc/xray/domain)
     SUB=$(tr </dev/urandom -dc a-z0-9 | head -c6)
-    SUB_DOMAIN=${SUB}.alvicell.my.id
+    SUB_DOMAIN=${SUB}."alvicell.my.id"
     NS_DOMAIN=ns.${SUB_DOMAIN}
-    CF_ID=adigemether@gmail.com
-    CF_KEY=897d4f161b3576bc1a5eaa9602453e246ce74
+    CF_ID="adigemether@gmail.com"
+    CF_KEY="897d4f161b3576bc1a5eaa9602453e246ce74"
     set -euo pipefail
     IP=$(wget -qO- ipinfo.io/ip)
     ZONE=$(
