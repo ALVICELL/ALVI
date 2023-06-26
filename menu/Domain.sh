7aa22eb737eeb3fb525382332cd3f59f0156a5c0
ALVICELL <136303773+ALVICELL@users.noreply.github.com>
2023-06-26 07:06:53 +0700

    Domain.sh

--- menu/Domain.sh
+++ menu/Domain.sh
@@ -55,13 +55,13 @@ function add-domain() {
     menu
 }
 add-ns() {
-    DOMAINNS="        .my.id"
+    DOMAINNS="alvicell.my.id"
     DAOMIN=$(cat /etc/xray/domain)
     SUB=$(tr </dev/urandom -dc a-z0-9 | head -c6)
-    SUB_DOMAIN=${SUB}."hendrabkn.my.id"
+    SUB_DOMAIN=${SUB}."alvicell.my.id"
     NS_DOMAIN=ns.${SUB_DOMAIN}
-    CF_ID=@gmail.com
-    CF_KEY=86431de017f7bf317c3960061da2f87c8effb
+    CF_ID=adigemether@gmail.com
+    CF_KEY=897d4f161b3576bc1a5eaa9602453e246ce74
     set -euo pipefail
     IP=$(wget -qO- ipinfo.io/ip)
     ZONE=$(
